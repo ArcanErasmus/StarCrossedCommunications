b745ac6579dba0dc74b8b8ace550b361542e5bb7
Conner Catanese <connerlcatanese@gmail.com>
2018-04-04 20:02:04 -0400

    help

---
+++ MicTest/Assets/Scripts/MicrophoneInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MicrophoneInput : MonoBehaviour
+{
+
+    public int audioSampleRate = 44100;
+    public string microphone;
+    public FFTWindow fftWindow;
+    public Dropdown micDropdown;
+    public Slider thresholdSlider;
+
+    private List<string> options = new List<string>();
+    private int samples = 8192;
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        // get components
+        audioSource = GetComponent<AudioSource>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ MicTest/Assets/Scripts/NewMicTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMicTest : MonoBehaviour
+{
+
+    public AudioSource source;
+
+    // Use this for initialization
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = Microphone.Start("Headset Microphone (4- SteelSeries Arctis 7 Chat)", false, 10, 44100);
+        source.Play();
+        //foreach (string device in Microphone.devices)
+        //{
+        //    Debug.Log(device);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

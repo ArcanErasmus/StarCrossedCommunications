46c283fd3fc079ae485dd2504bd164db446b100f
Marie Hoff <marieofthemoon@gmail.com>
2018-04-14 18:46:44 -0400

    no message

--- microphone-demo-master/Assets/Scripts/PlayerController.cs
+++ microphone-demo-master/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    float Xscale;
+    Vector3 velocity;
+
 	// Use this for initialization
 	void Start () {
-		
+        Xscale = transform.localScale.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey("a"))
+        {
+            transform.localScale.x = -Xscale;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.localScale.x = Xscale;
+        }
 	}
 }

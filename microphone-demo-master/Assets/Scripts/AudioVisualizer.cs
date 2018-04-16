37d1cf3a2d203fd847157531a17b77e640a5dd09
Kritapas Nimtrakul <kxn9945@rit.edu>
2018-04-16 04:36:56 -0400

    Score

--- microphone-demo-master/Assets/Scripts/AudioVisualizer.cs
+++ microphone-demo-master/Assets/Scripts/AudioVisualizer.cs
@@ -5,76 +5,89 @@ using System.Collections;
 public class AudioVisualizer : MonoBehaviour
 {
 
-    public Transform[] audioSpectrumObjects;
-    [Range(1, 100)] public float heightMultiplier;
-    [Range(64, 8192)] public int numberOfSamples = 1024; //step by 2
-    public FFTWindow fftWindow;
-    public float lerpTime = 1;
-    public Slider sensitivitySlider;
-    public Material pink, yellow;
+	public Transform[] audioSpectrumObjects;
+	[Range(1, 200)] public float heightMultiplier;
+	[Range(64, 8192)] public int numberOfSamples = 1024; //step by 2
+	public FFTWindow fftWindow;
+	public float lerpTime = 0.01f;
+	public Slider sensitivitySlider;
+	public Material pink, yellow;
 
-    private int highestBarHit = 0;
-    private float sensThreshold = 5.0f;
+	public int highestBarHit = 0;
+	private int curr = 0;
+	private float prevInten = 0.0f;
+	private float sensThreshold = 1.0f;
 
-    /*
+	/*
 	 * The intensity of the frequencies found between 0 and 44100 will be
 	 * grouped into 1024 elements. So each element will contain a range of about 43.06 Hz.
 	 * The average human voice spans from about 60 hz to 9k Hz
 	 * we need a way to assign a range to each object that gets animated. that would be the best way to control and modify animatoins.
 	*/
 
-    void Start()
-    {
+	void Start()
+	{
 
-        heightMultiplier = PlayerPrefsManager.GetSensitivity();
+		heightMultiplier = PlayerPrefsManager.GetSensitivity();
 
-        sensitivitySlider.onValueChanged.AddListener(delegate
-        {
-            SensitivityValueChangedHandler(sensitivitySlider);
-        });
-    }
+		sensitivitySlider.onValueChanged.AddListener(delegate
+			{
+				SensitivityValueChangedHandler(sensitivitySlider);
+			});
+	}
 
-    void Update()
-    {
+	void Update()
+	{
 
-        // initialize our float array
-        float[] spectrum = new float[numberOfSamples];
+		highestBarHit = 0;
+		// initialize our float array
+		float[] spectrum = new float[numberOfSamples];
 
-        // populate array with fequency spectrum data
-        GetComponent<AudioSource>().GetSpectrumData(spectrum, 0, fftWindow);
-        
-        // loop over audioSpectrumObjects and modify according to fequency spectrum data
-        // this loop matches the Array element to an object on a One-to-One basis.
-        for (int i = 0; i < audioSpectrumObjects.Length; i++)
-        {
+		// populate array with fequency spectrum data
+		GetComponent<AudioSource>().GetSpectrumData(spectrum, 0, fftWindow);
 
-            // apply height multiplier to intensity
-            float intensity = spectrum[i] * heightMultiplier;
 
-            // calculate object's scale
-            float lerpY = Mathf.Lerp(audioSpectrumObjects[i].localScale.y, intensity, lerpTime);
-            Vector3 newScale = new Vector3(audioSpectrumObjects[i].localScale.x, lerpY, audioSpectrumObjects[i].localScale.z);
+		prevInten = 0.0f;
+		// loop over audioSpectrumObjects and modify according to fequency spectrum data
+		// this loop matches the Array element to an object on a One-to-One basis.
 
-            // apply new scale to object
-            audioSpectrumObjects[i].localScale = newScale;
+		for (int i = 0; i < audioSpectrumObjects.Length; i++)
+		{
+			// apply height multiplier to intensity
+			float intensity = spectrum[i] * heightMultiplier;
+			Debug.Log (intensity);
 
-            // reset color of the object
-            audioSpectrumObjects[highestBarHit].GetComponent<MeshRenderer>().material = pink;
+			// calculate object's scale
+			float lerpY = Mathf.Lerp(audioSpectrumObjects[i].localScale.y, intensity, lerpTime);
+			Vector3 newScale = new Vector3(audioSpectrumObjects[i].localScale.x, lerpY, audioSpectrumObjects[i].localScale.z);
 
-            // see if there is a new highest bar hit
-            if (intensity > sensThreshold && i > highestBarHit)
-            {
-                highestBarHit = i;
-            }
-        }
+			// apply new scale to object
+			audioSpectrumObjects[i].localScale = newScale;
 
-        // set the color of the highest bar hit to yellow
-        audioSpectrumObjects[highestBarHit].GetComponent<MeshRenderer>().material = yellow;
-    }
+			// reset color of the object
+			audioSpectrumObjects[curr].GetComponent<MeshRenderer>().material = pink;
 
-    public void SensitivityValueChangedHandler(Slider sensitivitySlider)
-    {
-        heightMultiplier = sensitivitySlider.value;
-    }
+			// see if there is a new highest bar hit
+			if (intensity > sensThreshold && intensity > prevInten)
+			{
+				highestBarHit = i;
+				//Debug.Log (highestBarHit);
+			}
+
+			prevInten = intensity;
+		}
+
+
+
+		// set the color of the highest bar hit to yellow
+		audioSpectrumObjects[highestBarHit].GetComponent<MeshRenderer>().material = yellow;
+
+		curr = highestBarHit;
+	}
+
+	public void SensitivityValueChangedHandler(Slider sensitivitySlider)
+	{
+		heightMultiplier = sensitivitySlider.value;
+	}
 
 }
---
+++ microphone-demo-master/Assets/Scripts/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public AudioVisualizer micObj;
+	public AudioVisualizer instruObj;
+	public int playerscore = 0;
+	public Text mText;
+
+	// Use this for initialization
+	void Start () {
+		micObj = GameObject.Find ("Microphone").GetComponent("AudioVisualizer") as AudioVisualizer;
+		instruObj = GameObject.Find ("Music").GetComponent("AudioVisualizer") as AudioVisualizer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mText.text = "";
+		if ((micObj.highestBarHit != 0 && instruObj.highestBarHit != 0) && micObj.highestBarHit == instruObj.highestBarHit) {
+			playerscore++;
+		}
+		mText.text = playerscore+"";
+	}
+}
